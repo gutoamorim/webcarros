aa17d2af3e83fa44a8e0b06908a189a31f1e8df6
Cesar Augusto <gutoamorim@outlook.com>
2024-08-15 19:12:17 -0300

    feat: Add register and login pages

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,73 @@
+import { Container } from "../../components/container";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.string().email("Insira um e-mail válido"),
+  password: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logo} alt="logo do site" className="w-full" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu e-mail"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha"
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+          >
+            Acessar
+          </button>
+        </form>
+        <Link className="text-zinc-500" to="/register">
+          Ainda nao possui uma conta? Cadastre-se!
+        </Link>
+      </div>
+    </Container>
+  );
+};
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,83 @@
+import { Container } from "../../components/container";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3, "O campo nome é obrigatório."),
+  email: z.string().email("Insira um e-mail válido"),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres."),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logo} alt="logo do site" className="w-full" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="text"
+              placeholder="Digite seu nome"
+              name="name"
+              error={errors.name?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu e-mail"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha"
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+          >
+            Acessar
+          </button>
+        </form>
+        <Link className="text-zinc-500" to="/login">
+          Ja possui uma conta? Faça o login!
+        </Link>
+      </div>
+    </Container>
+  );
+};
