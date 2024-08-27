03eb2cd273813c7f704c98bd8690c7175c6ceced
Cesar Augusto <gutoamorim@outlook.com>
2024-08-27 10:47:56 -0300

    feat: render cars on user dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,110 @@
+import { useState, useEffect, useContext } from "react";
 import { Container } from "../../components/container";
 import { DashboardHeader } from "../../components/panelHeader";
+import { FiTrash2 } from "react-icons/fi";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { AuthContext } from "../../contexts/AuthContext";
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: ImagesCarProps[];
+  uid: string;
+}
+
+interface ImagesCarProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export const Dashboard = () => {
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    function loadCars() {
+      if (!user?.uid) {
+        return;
+      }
+
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, where("uid", "==", user.uid));
+
+      getDocs(queryRef).then((snapshot) => {
+        const listCars = [] as CarProps[];
+
+        snapshot.forEach((doc) => {
+          listCars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            uid: doc.data().uid,
+            images: doc.data().images,
+          });
+        });
+
+        setCars(listCars);
+      });
+    }
+
+    loadCars();
+  }, [user]);
+
+  async function handleDeleteCar(id: string) {
+    const docRef = doc(db, "cars", id);
+    await deleteDoc(docRef);
+    setCars(cars.filter((car) => car.id !== id));
+  }
+
   return (
     <Container>
       <DashboardHeader />
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {cars.map((car) => (
+          <section className="w-full bg-white rounded-lg relative" key={car.id}>
+            <button
+              onClick={() => handleDeleteCar(car.id)}
+              className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-4 drop-shadow"
+            >
+              <FiTrash2 size={26} color="#000" />
+            </button>
+            <img
+              src={car.images[0].url}
+              alt=""
+              className="w-full rounded-lg mb-2 max-h-70"
+            />
+            <p className="font-bold mt-1 px-2 mb-2">{car.name}</p>
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700">
+                Ano {car.year} | {car.km} km
+              </span>
+              <strong className="text-black font-bold">R$ {car.price}</strong>
+            </div>
+
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+
+            <div className="px-2 pb-2">
+              <span className="text-black">{car.city}</span>
+            </div>
+          </section>
+        ))}
+      </main>
     </Container>
   );
 };
